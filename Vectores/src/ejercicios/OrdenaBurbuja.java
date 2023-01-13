e68aedd15f33d8e914501c88f3c54e74ef697345
David <David@DESKTOP-HDONBHI>
2023-01-13 08:16:04 +0100

    añadidos los códigos de ordención

---
+++ Vectores/src/ejercicios/OrdenaBurbuja.java
@@ -0,0 +1,37 @@
+package ejercicios;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class OrdenaBurbuja {
+
+	public static void main(String[] args) {
+		/*
+		 * Programa que pide un vector por t3eclado y lo ordena
+		 *
+		 */
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Introduce el numero de elementos");
+		int tam = teclado.nextInt();
+		
+		int numeros[] = new int[tam];
+		System.out.println("Introduce el vector");
+		Vectores.pedir_vector(numeros);
+		
+		System.out.println("Tu vector es ");
+		Vectores.mostrar_vector(numeros);
+		
+		Vectores.ordenarBurbujaOptimizado(numeros);
+		
+		System.out.println("Tu vector ordenado es ");
+		Vectores.mostrar_vector(numeros);
+		
+		
+		
+		
+
+	}
+
+}
--- Vectores/src/ejercicios/Vectores.java
+++ Vectores/src/ejercicios/Vectores.java
@@ -1,6 +1,5 @@
 package ejercicios;
 
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class Vectores {
@@ -20,4 +19,54 @@ public class Vectores {
 		}
 		System.out.println();
 	}
+	
+	
+	public static void ordenarBurbuja(int v[]) {
+		int aux;
+		for(int i=0;i<v.length; i++) {
+			for (int j=0; j<v.length-1-i; j++) {
+				if (v[j]>v[j+1]) {
+					aux=v[j];
+					v[j]=v[j+1];
+					v[j+1]=aux;
+				}
+			}
+		}
+	}
+	public static void ordenarBurbujaOptimizado(int v[]) {
+		int aux;
+		boolean hayCambios=true;
+		for(int i=0;i<v.length && hayCambios; i++) {
+			hayCambios=false;
+			for (int j=0; j<v.length-1-i; j++) {
+				if (v[j]>v[j+1]) {
+					aux=v[j];
+					v[j]=v[j+1];
+					v[j+1]=aux;
+					hayCambios=true;
+				}
+			}
+		}
+	}
+	
+	public static void ordenarSeleccion(int vector[]) {
+		int menor;
+		int posMenor;
+		for (int i=0; i<vector.length-1;i++) {
+			menor=vector[i];
+			posMenor=i;
+			for (int j=i+1;j<vector.length;j++) {
+				if (vector[j]<menor) {
+					menor = vector[j];
+					posMenor=j;
+				}
+			}
+			vector[posMenor]=vector[i];
+			vector[i]=menor;
+		}
+	}
+	
+	
+	
+	
 }
